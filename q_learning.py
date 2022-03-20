e9a5bbcba54ddb55e458bb01749c70c9f0e41027
JuneyBoy <arjun.ganesan@ou.edu>
2022-03-19 22:20:32 -0500

    Formatted documents

--- q_learning.py
+++ q_learning.py
@@ -5,12 +5,13 @@ import numpy as np
 
 from plot_utils import *
 
-env = gym.make('Blackjack-v1')
-reward_dict = {'Losses': 0, 'Draws': 0, 'Wins': 0}
-action_dict = {0: 'stick', 1: 'hit'}
+env = gym.make("Blackjack-v1")
+reward_dict = {"Losses": 0, "Draws": 0, "Wins": 0}
+action_dict = {0: "stick", 1: "hit"}
+
 
 def q_learning(env, num_episodes, alpha=0.05, gamma=0.5):
-    '''
+    """
     Uses Q-Learning (pg 131 in textbook)
 
     Args:
@@ -21,7 +22,7 @@ def q_learning(env, num_episodes, alpha=0.05, gamma=0.5):
 
     Returns:
         value function Q: mapping (state, action) -> value
-    '''
+    """
 
     Q = defaultdict(lambda: np.zeros(env.action_space.n))
 
@@ -31,18 +32,23 @@ def q_learning(env, num_episodes, alpha=0.05, gamma=0.5):
         for i_step in range(100):
             if state in Q:
                 # if the state has been visited, then take the greedy action most of the time but take random action alpha % of the time
-                action = np.random.choice([np.argmax(Q[state]),0,1], p=[1-alpha, 0.5*alpha, 0.5*alpha])
+                action = np.random.choice(
+                    [np.argmax(Q[state]), 0, 1], p=[1 - alpha, 0.5 * alpha, 0.5 * alpha]
+                )
             else:
                 # if the state has not been visited, then take random action
-                action = np.random.choice([0,1])
+                action = np.random.choice([0, 1])
 
             next_state, reward, done, info = env.step(action)
 
-            Q[state][action] = Q[state][action] + alpha*(reward + gamma*np.argmax(Q[next_state]) - Q[state][action])
+            Q[state][action] = Q[state][action] + alpha * (
+                reward + gamma * np.argmax(Q[next_state]) - Q[state][action]
+            )
 
             state = next_state
 
-            if done: break
+            if done:
+                break
 
     return Q
 
